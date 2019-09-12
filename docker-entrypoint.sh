ff381eabc6b6db5d3e599a92cf7e3f0803dca2ca
William Jackson <william@subtlecoolness.com>
2019-09-12 10:37:27 -0500

    Protect variable expansion in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 /usr/local/bin/python /cartography/docker-generate-credentials.py
 # /usr/local/bin/cartography --neo4j-uri ${NEO4J_URI} --neo4j-user ${NEO4J_USER} --neo4j-password-env-var NEO4J_PASSWORD
-/usr/local/bin/cartography --neo4j-uri ${NEO4J_URI}
+/usr/local/bin/cartography --neo4j-uri "${NEO4J_URI}"
